42fdcff272c46896c57447d687ef26ca73ccfb21
Lies Tdj <liestidjedam@gmail.com>
2025-11-07 16:33:41 +0100

    Creation of the filter button Tous and the TodoItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import TodoItem from "./TodoItem";
 
 type Priority = "Urgente" | "Moyenne" | "Basse"
 
@@ -15,6 +16,7 @@ function App() {
   const savedTodos = localStorage.getItem("todos")
   const initialTodos = savedTodos ? JSON.parse(savedTodos) : []
   const [todos, setTodos] = useState<Todo[]>(initialTodos)
+  const [filter, setFilter] = useState<Priority | "Tous">("Tous")
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos))
@@ -35,7 +37,15 @@ function App() {
     setTodos(newTodos)
     setInput("")
     setPriority("Moyenne")
-    console.log("newTodos 4 : " + JSON.stringify(newTodos));
+    console.log("newTodos : " + JSON.stringify(newTodos));
+  }
+
+  let filteredTodos: Todo[] = []
+
+  if (filter == "Tous") {
+    filteredTodos = todos
+  } else {
+    filteredTodos = todos.filter(todo => todo.priority == filter)
   }
 
   return (
@@ -64,6 +74,30 @@ function App() {
           >
             Ajouter
             </button>
+        </div>
+        <div className="space-y-2 flex-1 h-fit">
+          <div className="flex flex-wrap gap-4">
+            <button
+              className={`btn btn-soft ${filter === "Tous" ? "btn-primary" : ""}`}
+              onClick={() => setFilter("Tous")}
+              >Tous
+            </button>
+            <button>Urgente</button>
+            <button>Moyenne</button>
+            <button>Basse</button>
+            
+          </div>
+          {filteredTodos.length > 0 ? (
+            <ul className="divide-y divide-primary/20">
+              {filteredTodos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoItem todo={todo} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>test 2</div>
+          )}
         </div>
       </div>
     </div>
